164ecb5863c670f46d7b1362a1a13cdfb9a11cdc
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-16 18:15:46 -0400

    pythonize

--- SingleImage.py
+++ SingleImage.py
@@ -1,10 +1,8 @@
 import pypylon
 from matplotlib.pyplot import figure,draw,pause
-
-print('PyPylon version:',pypylon.pylon_version.version)
-
-cams = pypylon.factory.find_devices()
-print(cams)
+#
+Nblock = 10  # TODO workaround for Basler too many frames error
+Nblocks = 100  # arbitrary number of frames to plot
 
 def connect(camdev,camind=0):
     
@@ -15,27 +13,38 @@ def connect(camdev,camind=0):
 
     return cam
 
-cam = connect(cams) 
+def simpleloop(cams):
 
-print('exposure time [millisec]:',cam.properties['ExposureTime']/1e3)
-# cam.properties['ExposureTime'] = 1000
+    cam = connect(cams) 
 
-cam.properties['DeviceLinkThroughputLimitMode'] = 'On'
+    print('exposure time [millisec]:',cam.properties['ExposureTime']/1e3)
+    # cam.properties['ExposureTime'] = 1000
 
-# %% movie
-ax = figure().gca()
-h = ax.imshow(list(cam.grab_images(1))[0])  # TODO workaround for grab_image() broken
-ht = ax.set_title('')
-cam.close()
+    # cam.properties['DeviceLinkThroughputLimitMode'] = 'On'
 
-Nblock = 10  # TODO workaround for Basler too many frames error
-for i in range(1000):
-
-    cam = connect(cams,0)
-
-    for j,I in enumerate(cam.grab_images(Nblock)):  # grab_images argument is how many images to yield
-        h.set_data(I)
-        ht.set_text('Image # {}'.format(i*Nblock+j))
-        draw(); pause(0.01)
-
+    # %% movie
+    ax = figure().gca()
+    h = ax.imshow(list(cam.grab_images(1))[0])  # TODO workaround for grab_image() broken
+    ht = ax.set_title('')
     cam.close()
+
+    for i in range(Nblocks):
+
+        cam = connect(cams,0)
+
+        for j,I in enumerate(cam.grab_images(Nblock)):  # grab_images argument is how many images to yield
+            h.set_data(I)
+            ht.set_text('Image # {} / {}'.format(i*Nblock+j,Nblocks*Nblock))
+            draw(); pause(0.01)
+
+        cam.close()
+
+
+if __name__ == '__main__':
+
+    print('Basler Pylon API version:',pypylon.pylon_version.version)
+
+    cams = pypylon.factory.find_devices()
+    print(cams)
+
+    simpleloop(cams)
